40b1498037a0dcee624f4427abd7647528002dfb
Bharat Kumar <k.bharatkumar815@gmail.com>
2025-07-27 02:29:59 +0530

    frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3003,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true
-      }
-    }
-  }
+        target: 'https://github-analyzer-bq96.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 }) 
